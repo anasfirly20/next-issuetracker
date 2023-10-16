f6d97cf3cb5555ca55cc15b4768a96f47d0f0d1a
Anas Firly <anasfirly20@gmail.com>
2023-10-16 11:53:05 +0700

    style: setting up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 export default function IssuesPage() {
   return (
     <section>
       <h1>ISSUES</h1>
+      <Button>NEW ISSUE</Button>
     </section>
   )
 }
